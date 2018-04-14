022a128a336a4c064179a2d9fa36b5e461a184e4
Mahesh Narkepatil <narkemahesh@yahoo.com>
2018-04-14 19:24:43 +0530

    Master

---
+++ src/main/java/com/onlykhel/OnlyKhel/OnlyKhelApplication.java
@@ -0,0 +1,12 @@
+package com.onlykhel.OnlyKhel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyKhelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyKhelApplication.class, args);
+	}
+}
---
+++ src/test/java/com/onlykhel/OnlyKhel/OnlyKhelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.onlykhel.OnlyKhel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlyKhelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
